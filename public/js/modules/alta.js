b1e312a49530daea685372a0694564bee31f156f
Ricardo Aschero <ricolico@hotmail.com>
2022-11-23 01:52:13 -0300

    Agregadas Rgexp

--- public/js/modules/alta.js
+++ public/js/modules/alta.js
@@ -1,7 +1,6 @@
 import productController from "../controllers/product.js";
 
 class PageAlta {
-    
     static productsTableContainer;
     static productForm;
     static fields;
@@ -9,50 +8,68 @@ class PageAlta {
     static btnUpdate;
     static btnCancel;
 
-    static validators = {
-        'id': /^[\da-f]{24}$/,
-        'name': /^[\wáéíóúüÁÉÍÓÚÜ .,-]{1,30}$/,
-        'price': /^\d+$/,
-        'description': /^[\wáéíóúüÁÉÍÓÚÜ ¿?¡!.,:-]{1,200}$/,
-    };
+    //static validators = {
+    //    id: /^[\da-f]{24}$/,
+    //    name: /^[\wáéíóúüÁÉÍÓÚÜ .,-]{1,30}$/,
+    //    price: /^\d+$/,
+    //    description: /^[\wáéíóúüÁÉÍÓÚÜ ¿?¡!.,:-]{1,200}$/,
+    //};
 
     static async init() {
         const inputs = document.querySelectorAll("input");
-        const regexGenerica = new RegExp(
-            /^[a-zA-ZÀ-ÿ0-9\s,\-\.\'\?\#\°\/]{1,20}$/
+        const regexNombre = new RegExp(/^[\wáéíóúüÁÉÍÓÚÜ .,-]{1,30}$/);
+        const regexPrecio = new RegExp(/^\d+$/);
+        const regexDescripcionLong = new RegExp(
+            /^[\wáéíóúüÁÉÍÓÚÜ ¿?¡!.,:-]{1,2000}$/
         );
+        const regexDescripcionShort = new RegExp(
+            /^[\wáéíóúüÁÉÍÓÚÜ ¿?¡!.,:-]{1,80}$/
+        );
+        const regexMarca = new RegExp(/^[\wáéíóúüÁÉÍÓÚÜ .,-]{1,40}$/);
         const submitButton = document.getElementById("submitButton");
-        const inputsAbc = [
-            inputs[1],
-            inputs[2],
-            inputs[3],
-            inputs[4],
-            inputs[5],
-            inputs[6],
-            inputs[7],
-            inputs[8],
+
+        const inputList = [
+            { input: inputs[1], regex: regexMarca },
+            { input: inputs[2], regex: regexNombre },
+            { input: inputs[3], regex: regexPrecio },
+            { input: inputs[4], regex: regexPrecio },
+            { input: inputs[5], regex: regexDescripcionShort },
+            { input: inputs[6], regex: regexDescripcionLong },
+            { input: inputs[7], regex: regexPrecio },
+            { input: inputs[8], regex: regexPrecio },
         ];
 
         const validate = (input, regexp) => {
             input.addEventListener("keydown", () => {
-                if (regexp.test(input.value)) {
-                    submitButton.removeAttribute("disabled");
-                } else {
+                if (!regexp.test(input.value)) {
                     submitButton.setAttribute("disabled", "true");
-                }
+                } 
             });
         };
         PageAlta.productsTableContainer = document.querySelector(
             ".products-table-container"
         );
-        inputsAbc.forEach((inputs) => {
-            for (let i = 0; i < inputsAbc.length; i++) {
-                validate(inputsAbc[i], regexGenerica);
-            }
+
+        inputList.forEach((field) => {
+            validate(field.input, field.regex);
         });
+        //document.addEventListener("keydown", () => {
+        //    validate(inputsAbc[1], regexMarca);
+        //    validate(inputsAbc[2], regexNombre);
+        //    validate(inputsAbc[3], regexPrecio);
+        //    validate(inputsAbc[4], regexPrecio);
+        //    validate(inputsAbc[5], regexDescripcionshort);
+        //    validate(inputsAbc[6], regexDescripcionLong);
+        //    validate(inputsAbc[7], regexPrecio);
+        //    validate(inputsAbc[8], regexPrecio);
+        //});
         PageAlta.loadTable();
         PageAlta.addTableEvents();
     }
+    //static validate(value, validator) {
+    //    return validator.test(value);
+    //}
+
     static async deleteProduct(e) {
         if (!confirm("¿Estás seguro de querer eliminar el producto?")) {
             return false;
@@ -85,14 +102,14 @@ class PageAlta {
         });
     }
 
-    static async renderTemplateTable(products) {
-        const hbsFile = await fetch("templates/products-table.hbs").then((r) =>
-            r.text()
-        );
-        const template = Handlebars.compile(hbsFile);
-        const html = template({ products });
-        PageAlta.productsTableContainer.innerHTML = html;
-    }
+    //static async renderTemplateTable(products) {
+    //    const hbsFile = await fetch("templates/products-table.hbs").then((r) =>
+    //        r.text()
+    //    );
+    //    const template = Handlebars.compile(hbsFile);
+    //    const html = template({ products });
+    //    PageAlta.productsTableContainer.innerHTML = html;
+    //}
 
     static prepareForm() {
         PageAlta.productForm = document.querySelector(".form-product");
@@ -124,14 +141,12 @@ class PageAlta {
         tablebody.innerHTML = "";
         products.forEach((product) => {
             const newtr = document.createElement("tr");
-            //newCard.classList.add("card");
             newtr.innerHTML = `
 <td data-product-property="id">${product.id}</td>
 <td data-product-property="name">${product.name}</td>
 <td data-product-property="price">$${product.price}</td>
 <td data-product-property="description">${product.description}</td>
 <td>
-<button title="Editar producto" class="btn-edit fa-solid fa-pen"></button>
 <button title="Eliminar producto" class="btn-delete fa-solid fa-trash"></button>
 </td>
 `;
@@ -141,7 +156,6 @@ class PageAlta {
     static objectIsEmpty(object) {
         return Object.entries(object).length === 0;
     }
-
 }
 
 export default PageAlta;
