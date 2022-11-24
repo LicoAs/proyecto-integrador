cc620c6d515e2a036abd8011f1a190ab9dc5343a
Ricardo Aschero <ricolico@hotmail.com>
2022-11-24 08:05:17 -0300

    js listo

---
+++ public/js/modules/nosotros.js
@@ -0,0 +1,14 @@
+
+console.warn('🆗: Módulo PageNosotros cargado.');
+
+class PageNosotros {
+
+    static async init () {
+        console.log('PageNosotros.init()');
+        console.log('.');
+        console.log('..');
+        console.log('...');
+    }
+}
+
+export default PageNosotros;
